f18f3804539f3631b72a3be51ac79650262a004f
Karim <karimsarkulov312@gmail.com>
2025-05-12 18:06:37 +0300

    Create Course

--- skillbridge-frontend/src/routes/AdminRoutes.jsx
+++ skillbridge-frontend/src/routes/AdminRoutes.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import AdminPage from "../pages/Admin/AdminPage";
 import AdminPanel from "../pages/Admin/AdminPanel";
 import RequireAdmin from "./RequireAdmin";
+import CreateCoursePage from "../pages/Admin/AdminPanel/CreateCoursePage";
+import EditCoursePage from "../pages/Admin/AdminPanel/EditCoursePage";
+import AdminUsersPage from "../pages/Admin/AdminUsers/AdminUsersPage";
 
 const AdminRoutes = () => {
   return (
@@ -9,6 +12,9 @@ const AdminRoutes = () => {
       <Route element={<RequireAdmin />}>
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/admin/panel" element={<AdminPanel />} />
+        <Route path="/admin/panel/course/create" element={<CreateCoursePage />} />
+        <Route path="/admin/panel/course/:courseId/edit" element={<EditCoursePage />} />
+        <Route path="/admin/panel/adminrole" element={<AdminUsersPage />} />
       </Route>
     </Routes>
   );
